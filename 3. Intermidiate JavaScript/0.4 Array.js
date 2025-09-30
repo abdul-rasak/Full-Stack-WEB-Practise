64b7b6e9603567738055927b081232607e1e28a5
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-09-30 08:03:07 +0100

    add javascript array file

---
+++ 3. Intermidiate JavaScript/0.4 Array.js
@@ -0,0 +1,12 @@
+//array 
+let pets_name = ["Rabbit", "Cat" ,"Dog"]
+console.log(pets_name);
+
+//Using indexing to select an array
+let my_second_pet = pets_name[1]
+console.log(my_second_pet);
+
+// Using the built in function lenght
+console.log(pets_name.length);
+console.log(my_second_pet.length);
+
