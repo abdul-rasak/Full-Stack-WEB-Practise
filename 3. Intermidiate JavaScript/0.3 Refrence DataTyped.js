0eb35a8a5c33d4e42561b5f9c3f65a259e91bb0e
Kamoru Samiat <kamorusamiatoyibusola@gmail.com>
2025-10-09 22:58:07 +0100

    modify the 0.3 Refrence DataTyped.js file

--- 3. Intermidiate JavaScript/0.3 Refrence DataTyped.js
+++ 3. Intermidiate JavaScript/0.3 Refrence DataTyped.js
@@ -5,3 +5,14 @@ let sister = {
 }
 
 console.log(sister);
+console.log(sister.name);
+console.log(sister.age);
+console.log(sister.nameOfInstitution);
+
+/* Different Approach */
+console.log(sister['name']);
+console.log(sister['age']);
+console.log(sister['nameOfInstitution']);
+
+let institution = 'nameOfInstitution'
+console.log(sister[institution]);
