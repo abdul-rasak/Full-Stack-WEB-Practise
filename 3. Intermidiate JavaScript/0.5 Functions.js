61596bb5da0a7d3d2e92b49c32636fe4ff203ff3
Kamoru Samiat <kamorusamiatoyibusola@gmail.com>
2025-10-10 16:49:19 +0100

    add functions.js file

---
+++ 3. Intermidiate JavaScript/0.5 Functions.js
@@ -0,0 +1,11 @@
+function greatings() {
+    console.log("Good Morning!"); 
+}
+
+greatings()
+
+// Implementing Parameters in Our function
+function greating(name) {
+     console.log("Good Morning " + name);    
+}
+greating("Abdulrasak")
