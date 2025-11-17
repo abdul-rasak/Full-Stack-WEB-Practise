8ebe854c2551c5e3f831f668e4596f420287406e
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-11-17 17:57:42 +0100

    add logical operator file

---
+++ 3. Intermidiate JavaScript/1.0 Logical Operators.js
@@ -0,0 +1,11 @@
+// OR Operator(||)
+console.log(1 =='1' || 1 === 1);
+console.log(true || false);
+console.log(false || false);
+console.log(false || true);
+
+// AND Operator(&&)
+console.log(1 =='1' && 1 === 1);
+console.log(true && false);
+console.log(false && false);
+console.log(true && true);
